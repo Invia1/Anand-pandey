a82e9f8919f2fb932b0d047dfa4f97029830a106
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-16 15:38:41 +0530

    Add files via upload

---
+++ Assignment6/UI/MainApp.cs
@@ -0,0 +1,41 @@
+﻿using System;using SmallEmployeeApp.Models;using SmallEmployeeApp.Service;namespace SmallEmployeeApp.UI{    public class MainApp    {        public static void Main()        {
+            Employee obj = new Employee();
+            obj.EID = 102;
+            obj.Ename = "Deep";
+            obj.Age = 22;
+
+            Employee obj1 = new Employee();
+            obj1.EID = 103;
+            obj1.Ename = "Deepa";
+            obj1.Age = 23;
+
+            Employee obj2 = new Employee();
+            obj2.EID = 104;
+            obj2.Ename = "Deepti";
+            obj2.Age = 24;
+
+            EmployeeService es = new EmployeeService();
+            es.CreateEmployee(obj);
+            es.CreateEmployee(obj1);
+            es.CreateEmployee(obj2);
+            Console.WriteLine("original list after adding element");
+            List<Employee> objList = es.GetEmployeeList();
+            foreach (var data in objList)
+            {
+              
+                Console.WriteLine(data.EID + " " + data.Ename + " " + data.Age);
+            }
+            Employee obj3 = new Employee();
+            obj3.EID = 105;
+            obj3.Ename = "Anand";
+            obj3.Age = 25;
+            es.UpdateEmployee(obj3);
+
+            
+
+            es.DeleteEmployee(obj1);
+
+
+        }
+
+    }}
---
+++ Assignment6/service/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using System;using SmallEmployeeApp.Models;namespace SmallEmployeeApp.Service{    public class EmployeeService    {        List<Employee> objList = new List<Employee>();
+        public int CreateEmployee(Employee emp)
+        {            objList.Add(emp);            return 1;
+        }
+
+        public List<Employee> GetEmployeeList()
+        {            return objList;
+        }        public void UpdateEmployee(Employee emp)
+        {
+            objList.Insert(0,emp);
+            Console.WriteLine(" list after inserting elemnt at index '0' ");
+            foreach (var data in objList)
+            {
+                Console.WriteLine(data.EID + " " + data.Ename + " " + data.Age);
+            }
+        }        public void DeleteEmployee(Employee emp)
+        {
+            objList.Remove(emp);
+            Console.WriteLine("list after deleting elemnet");
+            foreach (var data in objList)
+            {
+
+                Console.WriteLine(data.EID + " " + data.Ename + " " + data.Age);
+            }
+        }    }}
