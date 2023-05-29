e1874313bde1aa0165888789bfa52f9b8ab6a4ed
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-29 15:21:49 +0530

    Add files via upload
    predefined delegates like Func, Action and Predicate.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+class Practice
+{
+    public double salary()
+    {
+        return 12345.80;
+    }
+
+    public int WorkingHrs()
+    {
+        return 9;
+    }
+
+    public bool Attendence(int days)
+    {
+
+        if(WorkingHrs() == 9)
+        return true;
+
+        else
+            return false;
+    }
+
+    public void EmpType(string str)
+    {
+        Console.WriteLine("he is an IT Employee");
+    }
+}
+
+class MainApp
+{
+    public static void Main()
+    {
+        Practice obj = new Practice();
+        Func<double> obj1 = obj.salary;  // Implementing func delegate returnig double type
+        var res = obj1.Invoke();
+        Console.WriteLine(res);
+
+        Func<int> obj2 = obj.WorkingHrs;// Implementing func delegate returning int type
+        var temp = obj2.Invoke();
+        Console.WriteLine(temp);
+
+        Action<string > obj3 = obj.EmpType;  //Implementing Action delegate
+        obj3.Invoke("");
+
+        Predicate<int> obj4 = obj.Attendence; // Implementing Predicate delegate
+        bool attend = obj4.Invoke(9);
+        Console.WriteLine(attend);
+
+
+    }
+}
